74c7d76bb5a1489684c94fe2625a4c678e3b4168
kvacodes <kadiriavictor@gmail.com>
2024-07-26 07:39:44 +0100

    upload some projects

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,25 +1,62 @@
-import React from 'react'
+"use client";
+import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const HeroSection = () => {
-    return (
-        <section>
-            <div className='grid grid-cols-1 lg-grid-cols-12'>
-                <div className='col-span-7 place-self-center'>
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello there, I'm Victor</h1>
-                    <p className="text-[#ADB7BE] text-lg lg:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nunc nec ultricies.</p>
-                </div>
-            </div>
-            <div className='col-span-5'>
-                <Image
-                    src="/images/pf-pic-high.png"  
-                    alt="Hero Image"
-                    width={300}
-                    height={300}
-                />
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+              Hello!, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Victor",
+                1500,
+                "a Full stack Web Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={40}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Let's build something epic together!
+          </p>
+
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden">
+            <Image
+              src="/images/pf-pic-high.png"
+              alt="hero image"
+              className="object-cover"
+              style={{ objectPosition: '50% 30%' }}
+              fill
+              sizes="(max-width: 768px) 250px, 400px"
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
 export default HeroSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,106 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "React Portfolio Website",
+    description: "A fully interactive and responsive portfolio website built with next.js and framer-motion.",
+    image: "/images/projects/1.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  // {
+  //   id: 2,
+  //   title: "E-commerce Website",
+  //   description: "Project 2 description",
+  //   image: "/images/projects/2.png",
+  //   tag: ["All", "Web"],
+  //   gitUrl: "/",
+  //   previewUrl: "/",
+  // },
+  {
+    id: 3,
+    title: "Spotify API web application",
+    description: "Nostalgiwrap uses the spotify API to create a nostalgic user experience of their music taste from the past(Fully Responsive).",
+    image: "/images/projects/Nostalgiwrap.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/KVAcodes/Nostalgiwrap-too",
+    previewUrl: "https://nostalgiwrap-cce5d36cb37f.herokuapp.com/",
+  },
+  {
+    id: 4,
+    title: "The Slope Deflector",
+    description: "An entirely client-side web application that analysis continuous beams(fully responsive).",
+    image: "/images/projects/slopeDef.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/KVAcodes/The-Slope-Deflector",
+    previewUrl: "https://kvacodes.github.io/The-Slope-Deflector/#",
+  },
+
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+e        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
