7e69c7dd1e87d18e8565f0369fe9c2f384030240
kvacodes <kadiriavictor@gmail.com>
2024-07-26 13:03:20 +0100

    changed mail provider to mailjet

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      MAILJET_API_KEY: process.env.MAILJET_API_KEY,
+      MAILJET_API_SECRET: process.env.MAILJET_API_SECRET,
+      FROM_EMAIL: process.env.FROM_EMAIL,
+    },
+    // Add other Next.js config options here
+  };
+  
+  export default nextConfig;
--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,28 +1,45 @@
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
+import Mailjet from 'node-mailjet';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+const mailjet = new Mailjet({
+  apiKey: process.env.MAILJET_API_KEY,
+  apiSecret: process.env.MAILJET_API_SECRET
+});
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
+
   try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
-      subject: subject,
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
+    const result = await mailjet.post('send', { version: 'v3.1' }).request({
+      Messages: [
+        {
+          From: {
+            Email: process.env.FROM_EMAIL,
+            Name: "Kva portfolio"
+          },
+          To: [
+            {
+              Email: email,
+              Name: "Recipient"
+            }
+          ],
+          Subject: subject,
+          TextPart: `Thank you for contacting us!\n\nNew message submitted:\n${message}`,
+          HTMLPart: `
+            <h1>${subject}</h1>
+            <p>Thank you for contacting us!</p>
+            <p>New message submitted:</p>
+            <p>${message}</p>
+          `
+        }
+      ]
     });
-    return NextResponse.json(data);
+
+    console.log(result.body);
+    return NextResponse.json({ success: true });
   } catch (error) {
-    return NextResponse.json({ error });
+    console.error(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
